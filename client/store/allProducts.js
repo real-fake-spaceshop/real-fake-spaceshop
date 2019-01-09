8110816b2c9b3cbdd1ab75fa1e3c0608308cbad5
dboicourt <dboicourt@gmail.com>
2019-01-08 16:02:43 -0800

    feat(middleware):  Product Reducers
    Reducers for all products and single product
    
    resolve #26

---
+++ client/store/allProducts.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+
+//Action Types
+
+const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
+
+//Initial State
+
+const defaultProductList = [];
+
+//Action Creators
+
+const recAllProducts = products => ({type:GET_ALL_PRODUCTS, products});
+
+//Thunk Creator
+
+export const getAllProducts = () => {
+    return async dispatch => {
+        const response = await axios.get(`/api/products`)
+        dispatch(recAllProducts(response.data || defaultProductList))
+    }
+}
+
+//Reducer
+
+export default function(state= defaultProductList, action) {
+    switch (action.type) {
+        case GET_All_PRODUCTS:
+            return action.products
+        default:
+            return state
+    }
+}
