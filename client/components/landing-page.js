52ecc1e5228b31efcfe5d4824771429e6aeac12e
dboicourt <dboicourt@gmail.com>
2019-01-09 11:05:16 -0800

    feat(LandingPage): created basic landing page
    Siten defaults to landing page, buttons nonfunctional
    
    Resolves #31

---
+++ client/components/landing-page.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const LandingPage = () => {
+	return (
+		<div>
+			<div>
+				<h1>Real Fake Spaceships!</h1>
+			</div>
+			<div>
+				<img id="splash-logo" src="./images/default_ship_transparent.png" />
+			</div>
+			<div>
+				<button>Login</button>
+			</div>
+			<div>
+				<button>Sign Up</button>
+			</div>
+			<div>
+				<button>Product Catalogue</button>
+			</div>
+		</div>
+	);
+};
