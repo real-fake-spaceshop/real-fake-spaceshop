9c70b7c4236c067ae8a33b35b148315f24beb06e
Scott Techau <scotttechau@gmail.com>
2019-01-11 15:12:52 -0800

    feat(orders): Create API route to add products to an order.

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,5 +1,6 @@
+const db = require('../db');
 const router = require('express').Router();
-const {Order, Product} = require('../db/models');
+const {Order, Product, OrderLineItem} = require('../db/models');
 
 router.get('/:orderId', async (req, res, next) => {
 	try {
@@ -9,7 +10,67 @@ router.get('/:orderId', async (req, res, next) => {
 		if (order && req.user && req.user.id === order.ownerId) {
 			return res.json(order);
 		}
-		res.send(401).send('Unauthorized');
+		res.status(401).send('Unauthorized');
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.post('/', async (req, res, next) => {
+	try {
+		if (!req.user) {
+			return res.status(401).send('Unauthorized');
+		}
+
+		const {ownerId, products} = req.body;
+		const order = await Order.create({ownerId});
+		if (Array.isArray(products)) {
+			for (const product of products) {
+				order.addProduct(product.id, {
+					through: {quantity: product.quantity || 1}
+				});
+			}
+		}
+
+		const withProducts = await Order.findById(order.id, {include: [Product]});
+		res.status(201).json(withProducts);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.put('/:orderId/:productId', async (req, res, next) => {
+	try {
+		const order = await Order.findById(req.params.orderId, {
+			include: [Product]
+		});
+		if (!order) {
+			return next();
+		}
+
+		if (order.products.find(p => p.id === req.params.productId)) {
+			// need to update
+			if (!req.query.quantity) {
+				return res.status(400).json({error: 'quantity must be specified'});
+			}
+
+			console.log('updating product');
+			await OrderLineItem.update(
+				{quantity: req.query.quantity},
+				{where: req.params}
+			);
+		} else {
+			// need to link product to order
+			console.log(
+				`adding product ${req.params.productId} to order ${order.id}`
+			);
+			await order.addProduct(req.params.productId, {
+				through: {quantity: req.query.quantity || 1}
+			});
+		}
+
+		const withProducts = await Order.findById(order.id, {include: [Product]});
+		res.status(200).json(withProducts);
 	} catch (error) {
 		next(error);
 	}
--- server/api/orders.spec.js
+++ server/api/orders.spec.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-expressions */
 const {expect} = require('chai');
 const request = require('supertest');
 const db = require('../db');
 const app = require('../index');
-const {Order, OrderLineItem, Product, User} = require('../db/models');
+const {Order, Product, User} = require('../db/models');
 
 const server = request.agent(app);
 
@@ -13,30 +14,29 @@ const userData = {
 	address: 'somewhere'
 };
 
+const productsData = [
+	{name: 'Falcon 9', price: '100', imageUrl: '', description: 'none'},
+	{
+		name: 'Falcon Heavy',
+		price: '300',
+		imageUrl: '',
+		description: 'none'
+	},
+	{name: 'BFR', price: '1000', imageUrl: '', description: 'none'}
+];
 describe.only('Order API routes', () => {
 	let order, user, products;
 
 	beforeEach('clear database', () => {
+		console.log('clearing database');
 		return db.sync({force: true});
 	});
 
 	beforeEach('Add test order to database', async () => {
 		const userP = User.create(userData);
-		const productsP = Product.bulkCreate(
-			[
-				{name: 'Falcon 9', price: '100', imageUrl: '', description: 'none'},
-				{
-					name: 'Falcon Heavy',
-					price: '300',
-					imageUrl: '',
-					description: 'none'
-				},
-				{name: 'BFR', price: '1000', imageUrl: '', description: 'none'}
-			],
-			{
-				returning: true
-			}
-		);
+		const productsP = Product.bulkCreate(productsData, {
+			returning: true
+		});
 
 		[user, products] = await Promise.all([userP, productsP]);
 
@@ -62,20 +62,90 @@ describe.only('Order API routes', () => {
 				});
 		});
 
-		it('/api/orders/:orderId responds with order information', done => {
-			server
-				.get(`/api/orders/${order.id}`)
-				.expect(200)
-				.end(function(err, res) {
-					if (err || !res.ok) {
-						// console.error(err);
-						return done(err);
-					}
-					const body = res.body;
-					expect(body.ownerId).to.be.equal(user.id);
-					expect(body.products).to.have.length(products.length);
-					done();
+		describe('GET /api/orders/:orderId', () => {
+			it('responds with order information', done => {
+				server
+					.get(`/api/orders/${order.id}`)
+					.expect(200)
+					.end(function(err, res) {
+						if (err || !res.ok) {
+							// console.error(err);
+							return done(err);
+						}
+						const body = res.body;
+						expect(body.ownerId).to.be.equal(user.id);
+						expect(body.products).to.have.length(products.length);
+						done();
+					});
+			});
+		});
+
+		describe('POST /api/orders', () => {
+			it(' creates an order in the database', done => {
+				const newOrder = {
+					ownerId: user.id,
+					products: products.map(p => ({id: p.id, quantity: 1}))
+				};
+
+				server
+					.post('/api/orders')
+					.send(newOrder)
+					.expect(201)
+					.end(function(err, res) {
+						if (err || !res.ok) {
+							return done(err);
+						}
+						expect(res.body).to.be.an('object');
+						expect(res.body.id).to.exist;
+						Order.findById(res.body.id, {include: [Product]})
+							.then(createdOrder => {
+								expect(createdOrder).to.exist;
+								expect(createdOrder.products).to.be.an('array');
+								expect(createdOrder.products).to.have.length(products.length);
+								done();
+							})
+							.catch(error => done(error));
+					});
+			});
+		});
+
+		describe.only('PUT /api/orders/:orderId/:productId', () => {
+			let product;
+			beforeEach(async () => {
+				product = await Product.create({
+					name: 'Arwing',
+					description: 'none',
+					imageUrl: '',
+					price: 100000
 				});
+			});
+			it("Adds the product to the order if it doesn't exist", done => {
+				server
+					.put(`/api/orders/${order.id}/${product.id}?quantity=5`)
+					.expect(200)
+					.end(function(err, res) {
+						if (err || !res.ok) {
+							return done(err);
+						}
+						const updatedProduct = res.body.products.find(
+							p => p.id === product.id
+						);
+						expect(updatedProduct.order_line_item.quantity).to.equal(5);
+						order
+							.hasProduct(product)
+							.then(exists => {
+								expect(exists, 'order has product added to it').to.be.true;
+								done();
+							})
+							.catch(error => done(error));
+					});
+			});
+
+			it('Updates the product quantity if product is already on the order');
+		});
+
+		describe('DELETE /api/orders/:orderId/:productId', () => {
+			it('removes specified product from the order');
 		});
 	});
 
@@ -86,11 +156,9 @@ describe.only('Order API routes', () => {
 				.expect(401)
 				.end(function(err, res) {
 					if (err || (!res.ok && res.status !== 401)) {
-						console.log('**********************************');
 						return done(err);
 					}
 
-					console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 					return done();
 				});
 		});
